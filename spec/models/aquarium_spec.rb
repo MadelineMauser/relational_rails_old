ca7c6420855c95f83c1e10018b24521ae73a3f01
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-28 23:49:04 -0600

    Add test "Aquarium model exists"

--- spec/models/aquarium_spec.rb
+++ spec/models/aquarium_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Aquarium, type: :model do
-  
+  describe 'model existence' do
+    it 'exists' do
+      aquarium = Aquarium.create!(name: "River Haven", has_real_plants: false, gallons: 5.0, created_at: DateTime.now, updated_at: DateTime.now)
+
+      expect(aquarium).to be_an(Aquarium)
+    end
+  end
 end
