83209859b27c97c0d44101c583791529316719a3
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-28 22:15:23 -0600

    Add test "Shrimp model exists"

--- spec/models/shrimp_spec.rb
+++ spec/models/shrimp_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Shrimp, type: :model do
+  describe 'model existence' do
+    it 'exists' do
+      shrimp = Shrimp.create!
 
+      expect(shrimp).to be_a(Shrimp)
+    end
+  end
 end
