d76efd73b23d08737c4f6d60f578b748a68f55bd
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-29 01:03:07 -0600

    Fix Shrimp model test

--- spec/models/shrimp_spec.rb
+++ spec/models/shrimp_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Shrimp, type: :model do
   describe 'model existence' do
     it 'exists' do
-      shrimp = Shrimp.create!
+      shrimp = Shrimp.create!(name: "Mama Clementine", length_in_cm: 1.7, is_female: true, color: "orange", created_at: DateTime.now, updated_at: DateTime.now)
 
       expect(shrimp).to be_a(Shrimp)
     end
