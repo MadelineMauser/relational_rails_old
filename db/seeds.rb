56a84ce15d373406809e6d15b16579c814e4f0a3
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-30 19:34:57 -0600

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Shrimp.destroy_all
+Aquarium.destroy_all
+
+aquarium_1 = Aquarium.create!(name: "River Haven", has_real_plants: false, gallons: 5.0, created_at: DateTime.now, updated_at: DateTime.now)
+aquarium_2 = Aquarium.create!(name: "Waterfall Escape", has_real_plants: true, gallons: 10.0, created_at: DateTime.now, updated_at: DateTime.now)
+
+shrimp_1 = Shrimp.create!(name: "Mama Clementine", length_in_cm: 1.7, is_female: true, color: "orange", created_at: DateTime.now, updated_at: DateTime.now, aquarium_id: aquarium_1.id)
+shrimp_2 = Shrimp.create!(name: "Pimento Spice", length_in_cm: 1.3, is_female: false, color: "red", created_at: DateTime.now, updated_at: DateTime.now, aquarium_id: aquarium_2.id)
