c2201ea91014cfac007e9d0d95a47478d8feb65a
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-30 23:29:24 -0600

    Add Shrimps show spec file

---
+++ spec/features/shrimps/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'shrimps show page', type: :feature do
+  it 'can see the attributes of the given shrimp' do
+    aquarium_1 = Aquarium.create!(name: "River Haven", has_real_plants: false, gallons: 5.0, created_at: DateTime.now, updated_at: DateTime.now)
+
+    shrimp_1 = Shrimp.create!(name: "Mama Clementine", length_in_cm: 1.7, is_female: true, color: "orange", created_at: DateTime.now, updated_at: DateTime.now, aquarium_id: aquarium_1.id)
+
+    visit "/shrimps/#{shrimp_1.id}"
+
+    expect(page).to have_content(shrimp_1.name)
+    expect(page).to have_content("Aquarium ID: #{shrimp_1.aquarium_id}")
+    expect(page).to have_content("Is female: #{shrimp_1.is_female}")
+    expect(page).to have_content("Color: #{shrimp_1.color}")
+    expect(page).to have_content("Length: #{shrimp_1.length_in_cm} cm")
+    expect(page).to have_content("Created at: #{shrimp_1.created_at}")
+    expect(page).to have_content("Updated at: #{shrimp_1.updated_at}")
+  end
+end
