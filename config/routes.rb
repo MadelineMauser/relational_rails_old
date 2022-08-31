15a889361261a548e8b4e7584af864b91b1a260a
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-30 22:34:38 -0600

    Add Shrimps index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/aquaria', to: 'aquaria#index'
   get '/aquaria/:id', to: 'aquaria#show'
+  get '/shrimps', to: 'shrimps#index'
 end
